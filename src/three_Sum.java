e55769fc5868d409310def7316ad7e8e9b6513f3
Mayur25004 <mayurhotkar1447@gmail.com>
2026-07-04 17:15:22 +0530

    three sum problem

--- src/three_Sum.java
+++ src/three_Sum.java
@@ -1,3 +1,19 @@
-public class three_Sum {
+import java.util.ArrayList;
 
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<int<res>> = new ArrayList();
+
+        for(int i=0; i<nums.length;i++){
+            for(int j= i+1;j<nums.length; j++){
+                for(int k=j+1; k<nums.length; k++){
+
+                    if(nums[i]+nums[j]+nums[k]==0){
+                        res.add(nums[i]+nums[j]+nums[k]);
+                    }
+                }
+            }
+        }
+        return res;
+    }
 }
